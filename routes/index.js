d0ce5f805e30ecfe82bc1983973e0c14225b5172
steven <steven.loofat@yahoo.fr>
2019-06-09 16:38:37 +0800

    [+] Creating token + verify token

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,89 @@
-const express = require('express')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const jwt = require('jsonwebtoken');
 
-const app = express()
-const Router = express.Router()
+const app = express();
+const Router = express.Router();
 
+const privateKey = 'tomato';
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    if (username !== 'steven') {
+      return done(null, false, { message: 'Incorrect username' });
+    } else if (password !== 'epitech') {
+      return done(null, false, { message: 'Incorrect password' });
+    } else {
+      return done(null, { username, password });
+    }
+  })
+);
+
+const checkToken = (req, res, next) => {
+  const header = req.headers['authorization'];
+
+  if (typeof header !== undefined) {
+    const bearer = header.split(' ');
+
+    console.log('value of bearer ', bearer);
+
+    const token = bearer[1];
+
+    req.token = token;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+Router.get('/failure', (req, res) => {
+  res.status(403).send('/failure');
+});
+
+Router.get('/api/protected', checkToken, (req, res) => {
+  jwt.verify(req.token, privateKey, (err, authorizeData) => {
+    if (err) {
+      console.log('ERROR: Could not connect to the protected route');
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Only authorized user can see that message',
+        authorizeData //Content user Credential
+      });
+      console.log('SUCCESS: Connected ');
+    }
   });
 });
 
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    if (username !== 'steven') {
-      return done(null, false, { message: 'Incorrect username' })
-    }
-    else if (password !== 'epitech') {
-      return done(null, false, { message: 'Incorrect password' })
-    }
-    else {
-      return done(null, username)
-    }
-    // User.findOne({ username: username }, function (err, user) {
-    //   if (err) { return done(err); }
-    //   if (!user) {
-    //     return done(null, false, { message: 'Incorrect username.' });
-    //   }
-    //   if (!user.validPassword(password)) {
-    //     return done(null, false, { message: 'Incorrect password.' });
-    //   }
-    //   return done(null, user);
-    // });
-  }
-));
-
-Router.get('/', (req, res) => {
-  res.status(200).send('/')
-})
-
-Router.get('/failure', (req, res) => {
-  res.status(200).send('/failure')
-})
-
-Router.post('/login',
+Router.post(
+  '/login',
   passport.authenticate('local', {
-    successRedirect: '/',
     failureRedirect: '/failure',
     failureFlash: true
-  }), (req, res) => {
-    res.status(200).send('Inside the /login')
-  })
+  }),
+  (req, res) => {
+    jwt.sign(req.user, privateKey, { expiresIn: '1h' }, (err, token) => {
+      if (err) {
+        console.log('error ', err);
+      }
+      res.status(200).send(token);
+    });
+  }
+);
 
 module.exports = {
   Router
-}
+};
+
+// postman
+// header Authorization
+//value "Bearer " + token (take care of the space between Bearer and token)
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const flash = require('connect-flash')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const { Router } = require('./routes')
+const express = require('express');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+var session = require('express-session');
+const { Router } = require('./routes');
 
-const app = express()
-app.use(flash())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(Router)
+const app = express();
+app.use(
+  session({
+    cookie: { maxAge: 60000 },
+    secret: 'woot',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(flash());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(Router);
 
-app.listen(3000, () => { console.log('app running on port 3000 ') })
+app.listen(3000, () => {
+  console.log('app running on port 3000 ');
+});
 
-module.export = app
+module.export = app;
