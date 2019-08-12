9b5febaadf5210f8621482ec47a1dd509db0128f
steven <steven.loofat@yahoo.fr>
2019-08-12 20:44:55 +0800

    [+] getFighterByName() model update

--- model/fighter/fighterFunction.js
+++ model/fighter/fighterFunction.js
@@ -24,12 +24,10 @@ const getFighter = () => {
 }
 
 const getFighterByName = name => {
-  find({ name }, (err, fighter) => {
-    if (err) {
-      throw new Error('Failed find() in getFighterByName')
-    } else {
-      return fighter
-    }
+  return new Promise((resolve, reject) => {
+    Fighter.find({ name })
+      .then(fighter => resolve(fighter))
+      .catch(err => reject(err))
   })
 }
 
