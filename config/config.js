2fbaa969adcbbe379b72cb564c9563e26192ce55
steven <steven.loofat@yahoo.fr>
2019-08-13 21:39:03 +0800

    [+] config.js

---
+++ config/config.js
@@ -0,0 +1,31 @@
+const env = process.env.NODE_ENV
+
+const DEVELOPMENT = {
+  db: {
+    host: process.env.DATABASE_DEV,
+    user: process.env.DATABASE_DEV,
+    password: process.env.DB_PASSWORD_DEV
+  }
+}
+
+const PRODUCTION = {
+  db: {
+    host: process.env.DATABASE_PROD,
+    user: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD
+  }
+}
+
+const TEST = {
+  host: process.env.DATABASE_TEST,
+  user: process.env.DB_USER_TEST,
+  password: process.env.DB_PASSWORD_TEST
+}
+
+const config = {
+  DEVELOPMENT,
+  PRODUCTION,
+  TEST
+}
+
+module.exports = config[env]
