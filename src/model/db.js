8ec97b533e3d21fbabd1a0a69d48b22faf87c365
steven <steven.loofat@yahoo.fr>
2019-08-13 22:33:35 +0800

    [+] checking error in db.js

--- src/model/db.js
+++ src/model/db.js
@@ -4,13 +4,13 @@ const config = require('../../config/config') //why does it work
 const database =
   'mongodb://' + config.db.user + ':' + config.db.password + config.db.host
 
-const db = mongoose.connect(database)
+mongoose.connect(database)
 
-//doesn't work
+const db = mongoose.connection
 
-// db.on('error', () => {
-//   throw new Error('Connection to database failed')
-// })
+db.on('error', () => {
+  throw new Error('Connection to database failed')
+})
 
 module.exports = {
   db
