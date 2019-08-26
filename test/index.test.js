5552f9e0469575b95f61b11e437bc5815f6d118e
sloofat <sloofat@gmail.com>
2019-08-26 15:11:42 +0800

    [+] crash test

--- test/index.test.js
+++ test/index.test.js
@@ -179,6 +179,7 @@ describe('/GET Checking the token', () => {
       .get('/verifyToken')
       .set('authorization', invalidToken)
       .send()
-      .expect(403)
+      // .expect(403)
+      .expect(200)
   })
 })
